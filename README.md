98ca1f1170997957f0024c1c14b21b703eb6ff2a
Your Name <you@example.com>
2021-01-20 19:30:47 +0530

    deploy

---
+++ README.md
@@ -0,0 +1,19 @@
+# Assignment
+
+## Installation
+
+Use the cmd line to install dependencies. 
+
+```
+npm install
+```
+
+## Usage
+
+```
+npm run build
+npm run dev
+```
+
+
+
---
+++ client/src/data/index.js
@@ -0,0 +1,55 @@
+const data = [
+  {
+    id: 1,
+    icon: "📌",
+    status: "open",
+    title: "Learn React",
+    content: "Imporve your Knowledge",
+  },
+  {
+    id: 2,
+    icon: "📌",
+    status: "open",
+    title: "Contribute to Open Source !",
+    content: "Try programs like GSoc and MLH Fellowship",
+  },
+  {
+    id: 3,
+    icon: "📌",
+    status: "open",
+    title: "Devlope Community",
+    content: "Engage in Events and Workshops",
+  },
+  {
+    id: 4,
+    icon: "📌",
+    status: "open",
+    title: "Learn as much as you can !",
+    content: "Finish reading Intro to UI/UX",
+  },
+];
+
+const statuses = [
+  {
+    status: "open",
+    icon: "📌",
+    color: "#6E6E6E",
+  },
+  {
+    status: "in progress",
+    icon: "📈",
+    color: "#00C2E0",
+  },
+  {
+    status: "in review",
+    icon: "📝",
+    color: "#C377E0",
+  },
+  {
+    status: "done",
+    icon: "✅",
+    color: "#44CE48",
+  },
+];
+
+export { data, statuses };
